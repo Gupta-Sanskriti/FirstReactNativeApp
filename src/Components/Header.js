b02cf2b7926da5fce2c6f9f6ab60ac0f50c8e0f5
Sanskriti Gupta <sanskriti.gupta@letstransport.team>
2023-02-07 20:59:27 +0530

    first app

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const Header = () =>{
+    const [count, setCount] = useState(0);
+    return(
+        <Text>Hi</Text>
+    )
+}
+
+
+
+export default Header;
